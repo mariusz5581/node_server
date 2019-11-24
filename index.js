d23630e782760f64e8a72d8f0e3d8d868419d8c7
Mariusz <mariusz5581@gmail.com>
2019-11-24 14:10:19 +0200

    just update

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 var http = require('http');
-
+var fs = require('fs');
 
 http.createServer(function (req,res){
-    res.
-    res.write(indexFile);
-    res.end();
+    fs.readFile('index.html', function (err, data){
+        res.writeHead(200,{'Content-Type': 'text/html'});
+        res.write(data);
+        res.end();
+    });
 }).listen(8080);
