df1b667df29d0f8744709ca02769c6bae5c0b90e
Mariusz <mariusz5581@gmail.com>
2020-01-12 18:05:41 +0200

    Informative text on start

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 var http = require('http');
 var fs = require('fs');
 
+console.log('Starting server on port 80');
 http.createServer(function (req,res){
     fs.readFile('index.html', function (err, data){
         res.writeHead(200,{'Content-Type': 'text/html'});
